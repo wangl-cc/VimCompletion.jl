932e755d46546e2787397ce00b9e5f860a50d24c
Long Wang <wangl.cc@outlook.com>
2019-03-24 22:22:36 +0800

    Fix bug in latex_symbol completion.

--- src/complete.jl
+++ src/complete.jl
@@ -1,6 +1,27 @@
 using REPL.REPLCompletions
 using JSON
 
+"""
+    findstart(line::AbstractString,pos::Int)::Int
+
+Find complete start position of given string by recognize some separators. Because "using " in REPLCompletions is flag of package, which will be different. 
+
+# Arguments
+- `line::AbstractString`: A string contain chars from current line.
+- `pos::Int`: Current cursor's col number.
+
+# Example
+```jldoctest
+julia> findstart("using ", 6)
+0
+
+julia> findstart("func(arg", 6)
+5
+
+julia> findstart("1+arg", 6)
+2
+```
+"""
 function findstart(line::AbstractString, pos::Int)::Int
     if pos == 1
         return 0
@@ -17,16 +38,49 @@ function findstart(line::AbstractString, pos::Int)::Int
             start = tmpstart
         end
     end
+    tmpstart = findprev(isequal('\\'), line, pos-1)
+    tmpstart = isnothing(tmpstart) ? 0 : tmpstart-1
+    if tmpstart >= start
+        start = tmpstart
+    end
     return start
 end
 
+"""
+    getcompletion(base::AbstractString,pos::Int,context_module=Main)
+
+By calling function completions in REPLCompletions get the completions of give str `base`.
+
+# Arguments
+- `base::AbstractString`: The string is to be complete.
+- `pos::Int`: Current current col number.
+- `context_module=Main`: Context module of completion.
+
+# Example
+```jldoctest
+julia> getcompletion("using ", 6)
+(["using Atom", "using BSON", "using Base", "using Base64", "using BenchmarkTools"
+, "using CRC32c", "using CUDAdrv", "using CodecZlib", "using Conda", "using Core"
+ …  "using Serialization", "using SharedArrays", "using Sockets", "using SparseArr
+ays", "using Statistics", "using SuiteSparse", "using Test", "using UUIDs", "using
+ Unicode", "using VimCompletion"], true)
+
+julia> getcompletion("pri", 3)
+(["primitive type", "print", "println", "printstyled"], true)
+
+julia> getcompletion("Base.", 5)
+(["Base.!", "Base.!=", "Base.!==", "Base.%", "Base.&", "Base.*", "Base.+", "Base.-
+", "Base./", "Base.//"  …  "Base.≢", "Base.≤", "Base.≥", "Base.⊆", "Base.⊇", "Base
+.⊈", "Base.⊉", "Base.⊊", "Base.⊋", "Base.⊻"], true)
+```
+"""
 function getcompletion(base::AbstractString, pos::Int, context_module=Main)
     ret, range, should_complete = completions(base, pos, context_module)
     completionlist = unique(completion_text.(ret))
     if pos == 0
         return completionlist, should_complete
     end
-    for i in ('.', '`', '"', '\\', '/', ' ')
+    for i in ('.', '`', '"', '/', ' ')
         symbolpos = findprev(isequal(i), base, pos)
         if !isnothing(symbolpos)
             completionlist =  base[1:symbolpos] .* completionlist
